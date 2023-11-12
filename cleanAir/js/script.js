55a90f11381b0f47a79c1c4c94d8d2fc63e362b6
smeller1am <petrovarvic@gmail.com>
2023-11-13 00:17:35 +0300

    initial commit

---
+++ cleanAir/js/script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+      const swiper = new Swiper('.sertificate__swiper', {
+        slidesPerView: 1,
+        // spaceBetween: 20,
+        // centeredSlides: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+          },
+    
+      });
+      $('.burger').on('click', () => {
+        $('.menu').addClass('menu--active')
+        $('.overlay').addClass('overlay--active')
+        setTimeout(()=> {
+            $('.overlay').addClass('overlay--opacity')
+        }, 100)
+      })
+      $('.menu__back').on('click', () => {
+        $('.menu').removeClass('menu--active')
+        $('.overlay').removeClass('overlay--active')
+        setTimeout(()=> {
+            $('.overlay').removeClass('overlay--opacity')
+        }, 100)
+      })
+      $('.overlay').on('click', () => {
+        $('.menu').removeClass('menu--active')
+        $('.overlay').removeClass('overlay--active')
+        setTimeout(()=> {
+            $('.overlay').removeClass('overlay--opacity')
+        }, 100)
+      })
+      $('.menu__wrapper').on('click', function() {
+        $('.menu__wrapper').not(this).next().slideUp()
+        $(this).next().slideToggle()
+      })
+      if(window.innerWidth >= 576) {
+        let container = document.querySelector('.container')
+        let menu = document.querySelector('.menu')
+        const compStyles = window.getComputedStyle(container);
+        console.log();
+        menu.style.paddingRight = `${Number(compStyles.marginRight.replace('px', '')) + 20}px`
+      window.addEventListener('resize', () => {
+        let container = document.querySelector('.container')
+        let menu = document.querySelector('.menu')
+        const compStyles = window.getComputedStyle(container);
+        console.log();
+        menu.style.paddingRight = `${Number(compStyles.marginRight.replace('px', '')) + 20}px`
+      })
+      }
+});
