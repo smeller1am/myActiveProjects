d706a5dab33eada473b4344ab27cc1f2a139eece
smeller1am <petrovarvic@gmail.com>
2023-12-11 02:37:52 +0300

    11.12 added

--- metaId/js/script.js
+++ metaId/js/script.js
@@ -7,10 +7,43 @@ document.addEventListener('DOMContentLoaded', function () {
   $(".bottom__chat").mCustomScrollbar({
     axis : 'y'
   });
-  document.querySelector('.bottom__forum-content').addEventListener('click', () => {
+  if(window.innerWidth < 993){
+    $(".news__content").mCustomScrollbar({
+      axis : 'y'
+    });
+    $(".news__content").mCustomScrollbar({
+      axis : 'y'
+    });
+  } else {
+    $(".news__right").mCustomScrollbar({
+      axis : 'y'
+    });
+    $(".news__box").mCustomScrollbar({
+      axis : 'y'
+    });
+  }
+  $('.fancy__close').on('click', ()=> {
+    $.fancybox.close();
+  })
+  document.querySelector('.bottom__forum ').addEventListener('click', () => {
     document.querySelector('.bottom__forum-dropdown').classList.toggle('bottom__forum-dropdown--active')
+    document.querySelector('.info').classList.toggle('info--index')
   })
   document.querySelector('.close').addEventListener('click', () => {
     document.querySelector('.info__comment').classList.add('info__comment--hidden')
   })
+
+    document.querySelector('.header__burger').addEventListener('click', () => {
+      document.querySelector('.menu').classList.toggle('menu--active')
+    })
+  if(window.innerWidth < 1480){
+    document.querySelector('#headerBack').src = './assets/img/Group 307163758.png'
+    document.querySelector('#commentImg').src = './assets/img/Rectangle 614020.png'
+  }
+  if(window.innerWidth < 500){
+    document.querySelector('.bottom > img').src = './assets/img/Group 307163759.png'
+  }
+  if(window.innerWidth < 768){
+    document.querySelector('.bottom__forum-dropdown > img').src = './assets/img/Vector 45.png'
+  }
 });
