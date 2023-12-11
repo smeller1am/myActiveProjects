b443aa9648ad7561017b6e068d3af33a4cb607d3
smeller1am <petrovarvic@gmail.com>
2023-12-12 00:41:13 +0300

    prefinal push

--- metaId/js/script.js
+++ metaId/js/script.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.addEventListener('swiped', function(e) {
-    console.log(e.target); // element that was swiped
-    console.log(e.detail); // see event data below
-    console.log(e.detail.dir); // swipe direction
+  document.querySelector('.bottom__forum ').addEventListener('swiped-up', function(e) {
+    document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--active')
+
+    if(window.innerWidth > 769){
+      document.querySelector('.info').classList.add('info--index')
+    } else{
+      document.querySelector('.info').classList.add('info--opacity')
+    }
+    document.querySelector('.bottom__forum > svg').classList.add('active')
+  });
+  document.querySelector('.bottom__forum-dropdown > svg ').addEventListener('swiped-down', function(e) {
+    document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--active')
+    if(window.innerWidth > 769){
+      document.querySelector('.info').classList.remove('info--index')
+    } else{
+      document.querySelector('.info').classList.remove('info--opacity')
+    }
+    document.querySelector('.bottom__forum > svg').classList.remove('active')
   });
   $(".bottom__chat").mCustomScrollbar({
     axis : 'y'
@@ -27,7 +41,35 @@ document.addEventListener('DOMContentLoaded', function () {
   })
   document.querySelector('.bottom__forum ').addEventListener('click', () => {
     document.querySelector('.bottom__forum-dropdown').classList.toggle('bottom__forum-dropdown--active')
-    document.querySelector('.info').classList.toggle('info--index')
+    document.querySelector('.bottom__forum ').classList.toggle('bottom__forum--index')
+    document.querySelector('.bottom__forum-content ').classList.toggle('bottom__forum-content--index')
+    document.querySelector('.bottom__forum .wrapper1 > img ').classList.toggle('index')
+    setTimeout(() => {
+      document.querySelector('.bottom__forum-dropdown').classList.toggle('bottom__forum-dropdown--index')
+    }, 100)
+    if(window.innerWidth > 769){
+      document.querySelector('.info').classList.toggle('info--index')
+    } else{
+      document.querySelector('.info').classList.toggle('info--opacity')
+    }
+    document.querySelector('.bottom__forum .wrapper1 > svg').classList.toggle('active')
+    if(window.innerWidth > 501){
+      if(document.querySelector('.bottom > img').src.includes("307163755.png") ){
+        setTimeout(() => {
+          document.querySelector('.bottom > img').src = "./assets/img/Vector 452.png" 
+        }, 100)
+      } else{
+        document.querySelector('.bottom > img').src = "./assets/img/Group 307163755.png"
+      }
+    } else{
+      if(document.querySelector('.bottom > img').src.includes("307163759.png") ){
+        setTimeout(() => {
+          document.querySelector('.bottom > img').src = "./assets/img/Vector 451.png" 
+        }, 100)
+      } else{
+        document.querySelector('.bottom > img').src = "./assets/img/Group 307163759.png"
+      }
+    }
   })
   document.querySelector('.close').addEventListener('click', () => {
     document.querySelector('.info__comment').classList.add('info__comment--hidden')
