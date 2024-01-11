f8d28bb6f7178d5713c40d18c03736a2d8ba8cd5
smeller1am <petrovarvic@gmail.com>
2024-01-12 01:57:25 +0400

    css fixes

--- metaId/js/script.js
+++ metaId/js/script.js
@@ -12,28 +12,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     document.querySelector('.bottom__forum > .wrapper1 svg').classList.add('active')
     if (window.innerWidth > 501) {
+      document.querySelector('.bottom__backg').classList.add('bottom__backg--alt')
       document.querySelector('.bottom').classList.remove('bottom--bottom1')
       document.querySelector('.bottom').classList.add('bottom--bottom')
-      if (document.querySelector('.bottom > img').src.includes("4521.png")) {
-        setTimeout(() => {
-          document.querySelector('.bottom > img').src = "./assets/img/Vector 4522.png"
 
-        }, 200)
-      } else {
-        console.log(2);
-        // document.querySelector('.bottom > img').src = "./assets/img/Vector 452.png"
-        document.querySelector('.bottom > img').src = "./assets/img//Vector 4521.png"
-        document.querySelector('.bottom > img').classList.remove('left')
-      }
     } else {
-      if (document.querySelector('.bottom > img').src.includes("307163759.png")) {
-        setTimeout(() => {
-          document.querySelector('.bottom > img').src = "./assets/img/Vector 452.png"
-          document.querySelector('.bottom').classList.add('bottom--bottom')
-        }, 100)
-      } else {
-        document.querySelector('.bottom > img').src = "./assets/img/Group 307163759.png"
-      }
+      document.querySelector('.bottom__backg').classList.add('bottom__backg--alt')
+      
     }
   });
   ymaps.ready(function () {
@@ -73,10 +58,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   });
   if (window.innerWidth > 992 || window.innerWidth < 769) {
-    document.querySelector('.vizit__dropdown-visible').addEventListener('click', function () {
-      this.parentNode.classList.toggle('vizit__dropdown--active')
-      $('.vizit__dropdown-content').slideToggle()
-    })
+    if(document.querySelector('.vizit__dropdown-visible')){
+      document.querySelector('.vizit__dropdown-visible').addEventListener('click', function () {
+        this.parentNode.classList.toggle('vizit__dropdown--active')
+        $('.vizit__dropdown-content').slideToggle()
+      })
+    }
   }
   document.querySelector('.header__dropdown-bottom').addEventListener('click', function () {
 
@@ -99,8 +86,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     document.querySelector('.bottom__forum .wrapper1 > svg').classList.remove('active')
     if (window.innerWidth > 501) {
-      document.querySelector('.bottom > img').src = "./assets/img//Vector 4521.png"
-      document.querySelector('.bottom > img').classList.remove('left')
+      document.querySelector('.bottom__backg').classList.remove('bottom__backg--alt')
+
       // if (document.querySelector('.bottom > img').src.includes("307163755.png")) {
       //   console.log(11);
 
@@ -114,15 +101,7 @@ document.addEventListener('DOMContentLoaded', function () {
       //   document.querySelector('.bottom').classList.add('bottom--bottom1')
       // }
     } else {
-      if (document.querySelector('.bottom > img').src.includes("307163759.png")) {
-        setTimeout(() => {
-          document.querySelector('.bottom > img').src = "./assets/img/Vector 452.png"
-
-        }, 100)
-      } else {
-        document.querySelector('.bottom > img').src = "./assets/img/Group 307163759.png"
-        document.querySelector('.bottom').classList.remove('bottom--bottom')
-      }
+      document.querySelector('.bottom__backg').classList.remove('bottom__backg--alt')
     }
   });
   if (window.innerWidth < 992) {
@@ -216,7 +195,7 @@ document.addEventListener('DOMContentLoaded', function () {
     $.fancybox.close();
   })
   if (window.innerWidth > 992) {
-    document.querySelector('.bottom > img ').addEventListener('click', () => {
+    document.querySelector('.bottom > img ')?.addEventListener('click', () => {
 
       // document.querySelector('.bottom__forum-content ').classList.remove('bottom__forum-content--index')
       // document.querySelector('.bottom__forum .wrapper1 > img ').classList.remove('index')
@@ -344,74 +323,60 @@ document.addEventListener('DOMContentLoaded', function () {
       }
       document.querySelector('.bottom__forum .wrapper1 > svg').classList.toggle('active')
       if (window.innerWidth > 501) {
-        if (document.querySelector('.bottom > img').src.includes("4521.png")) {
-          setTimeout(() => {
-            document.querySelector('.bottom > img').src = "./assets/img/Vector 4522.png"
-            document.querySelector('.bottom > img').classList.add('left')
-          }, 100)
-        } else {
-          // document.querySelector('.bottom > img').src = "./assets/img/Vector 4521.png"
-        }
+       
       } else {
-        if (document.querySelector('.bottom > img').src.includes("307163759.png")) {
-          setTimeout(() => {
-            document.querySelector('.bottom').classList.add('bottom--bottom')
-            document.querySelector('.bottom > img').src = "./assets/img/Vector 452.png"
-          }, 100)
-        } else {
-          document.querySelector('.bottom > img').src = "./assets/img/Group 307163759.png"
-        }
+        
       }
     })
-    document.querySelector('.bottom > img ').addEventListener('click', () => {
-      document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--active')
-      document.querySelector('.bottom__forum-content ').classList.remove('bottom__forum-content--index')
-      document.querySelector('.bottom__forum .wrapper1 > img ').classList.remove('index')
-      // document.querySelector('.bottom > img').classList.remove('vis')
-      if (!document.querySelector('.bottom__forum-dropdown--active')) {
+    // document.querySelector('.bottom > img ')?.addEventListener('click', () => {
+    //   document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--active')
+    //   document.querySelector('.bottom__forum-content ').classList.remove('bottom__forum-content--index')
+    //   document.querySelector('.bottom__forum .wrapper1 > img ').classList.remove('index')
+    //   // document.querySelector('.bottom > img').classList.remove('vis')
+    //   if (!document.querySelector('.bottom__forum-dropdown--active')) {
 
-        document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--index')
-        // document.querySelector('.bottom > img').classList.add('vis')
-        setTimeout(() => {
-          document.querySelector('.bottom__forum ').classList.remove('bottom__forum--index')
-        }, 150)
-      } else {
-        document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--index')
+    //     document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--index')
+    //     // document.querySelector('.bottom > img').classList.add('vis')
+    //     setTimeout(() => {
+    //       document.querySelector('.bottom__forum ').classList.remove('bottom__forum--index')
+    //     }, 150)
+    //   } else {
+    //     document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--index')
 
-        setTimeout(() => {
-          document.querySelector('.bottom__forum ').classList.add('bottom__forum--index')
-        }, 100)
-      }
+    //     setTimeout(() => {
+    //       document.querySelector('.bottom__forum ').classList.add('bottom__forum--index')
+    //     }, 100)
+    //   }
 
-      if (window.innerWidth > 769) {
-        document.querySelector('.info').classList.remove('info--index')
-      } else {
-        document.querySelector('.info').classList.remove('info--opacity')
-      }
-      document.querySelector('.bottom__forum .wrapper1 > svg').classList.remove('active')
-      if (window.innerWidth > 501) {
-        if (document.querySelector('.bottom > img').src.includes("4521.png")) {
-          setTimeout(() => {
-            document.querySelector('.bottom > img').src = "./assets/img/Vector 4522.png"
+    //   if (window.innerWidth > 769) {
+    //     document.querySelector('.info').classList.remove('info--index')
+    //   } else {
+    //     document.querySelector('.info').classList.remove('info--opacity')
+    //   }
+    //   document.querySelector('.bottom__forum .wrapper1 > svg').classList.remove('active')
+    //   if (window.innerWidth > 501) {
+    //     if (document.querySelector('.bottom > img').src.includes("4521.png")) {
+    //       setTimeout(() => {
+    //         document.querySelector('.bottom > img').src = "./assets/img/Vector 4522.png"
 
-          }, 200)
-        } else {
-          document.querySelector('.bottom > img').src = "./assets/img//Vector 4521.png"
-          document.querySelector('.bottom > img').classList.remove('left')
+    //       }, 200)
+    //     } else {
+    //       document.querySelector('.bottom > img').src = "./assets/img//Vector 4521.png"
+    //       document.querySelector('.bottom > img').classList.remove('left')
 
-        }
-      } else {
-        if (document.querySelector('.bottom > img').src.includes("307163759.png")) {
-          // setTimeout(() => {
-          //   document.querySelector('.bottom > img').src = "./assets/img/Vector 451.png"
+    //     }
+    //   } else {
+    //     if (document.querySelector('.bottom > img').src.includes("307163759.png")) {
+    //       // setTimeout(() => {
+    //       //   document.querySelector('.bottom > img').src = "./assets/img/Vector 451.png"
 
-          // }, 100)
-        } else {
-          document.querySelector('.bottom').classList.remove('bottom--bottom')
-          document.querySelector('.bottom > img').src = "./assets/img/Group 307163759.png"
-        }
-      }
-    })
+    //       // }, 100)
+    //     } else {
+    //       document.querySelector('.bottom').classList.remove('bottom--bottom')
+    //       document.querySelector('.bottom > img').src = "./assets/img/Group 307163759.png"
+    //     }
+    //   }
+    // })
   }
   document.querySelector('.close').addEventListener('click', () => {
     document.querySelector('.info__comment').classList.add('info__comment--hidden')
@@ -420,13 +385,13 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.header__burger').addEventListener('click', () => {
     document.querySelector('.menu').classList.toggle('menu--active')
   })
-  if (window.innerWidth < 1480) {
-    document.querySelector('#headerBack').src = './assets/img/Group 307163758.png'
-    document.querySelector('#commentImg').src = './assets/img/Rectangle 614020.png'
-  }
-  if (window.innerWidth < 500) {
-    document.querySelector('.bottom > img').src = './assets/img/Group 307163759.png'
-  }
+  // if (window.innerWidth < 1480) {
+  //   document.querySelector('#headerBack').src = './assets/img/Group 307163758.png'
+  //   document.querySelector('#commentImg').src = './assets/img/Rectangle 614020.png'
+  // }
+  // if (window.innerWidth < 500) {
+  //   document.querySelector('.bottom > img').src = './assets/img/Group 307163759.png'
+  // }
   if (window.innerWidth < 768) {
     document.querySelector('.bottom__forum-dropdown > img').src = './assets/img/Vector 45.png'
   }
@@ -590,105 +555,83 @@ document.addEventListener('DOMContentLoaded', function () {
   mc1.on('pandown', function () {
     document.querySelector('.header__dropdown').classList.add('header__dropdown--active')
   })
-  mc2.on('panup', function () {
-    document.querySelector('.bottom__map-shtor').classList.add('hidden')
-    document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--active')
-    document.querySelector('.bottom__forum-content ').classList.add('bottom__forum-content--index')
-    document.querySelector('.bottom__forum .wrapper1 > img ').classList.add('index')
-    // document.querySelector('.bottom > img').classList.remove('vis')
-    if (!document.querySelector('.bottom__forum-dropdown--active')) {
-
-      // document.querySelector('.bottom > img').classList.add('vis')
-      document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--index')
-      setTimeout(() => {
-        document.querySelector('.bottom__forum ').classList.remove('bottom__forum--index')
-      })
-    } else {
-      document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--index')
-      document.querySelector('.bottom__forum ').classList.add('bottom__forum--index')
-      setTimeout(() => {
-
-      }, 100)
-    }
-    console.log(document.querySelector('.bottom__forum--index'));
-    if (window.innerWidth > 769) {
-      document.querySelector('.info').classList.add('info--index')
-    } else {
-      document.querySelector('.info').classList.toggle('info--opacity')
-    }
-    document.querySelector('.bottom__forum .wrapper1 > svg').classList.toggle('active')
-    if (window.innerWidth > 501) {
-      if (document.querySelector('.bottom > img').src.includes("4521.png")) {
+  if(window.innerWidth > 992){
+    mc2.on('panup', function () {
+      document.querySelector('.bottom__map-shtor').classList.add('hidden')
+      document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--active')
+      document.querySelector('.bottom__forum-content ').classList.add('bottom__forum-content--index')
+      document.querySelector('.bottom__forum .wrapper1 > img ').classList.add('index')
+      // document.querySelector('.bottom > img').classList.remove('vis')
+      if (!document.querySelector('.bottom__forum-dropdown--active')) {
+  
+        // document.querySelector('.bottom > img').classList.add('vis')
+        document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--index')
         setTimeout(() => {
-          document.querySelector('.bottom > img').src = "./assets/img/Vector 4522.png"
-          document.querySelector('.bottom > img').classList.add('left')
-        }, 100)
+          document.querySelector('.bottom__forum ').classList.remove('bottom__forum--index')
+        })
       } else {
-        // document.querySelector('.bottom > img').src = "./assets/img/Vector 4521.png"
-      }
-    } else {
-      if (document.querySelector('.bottom > img').src.includes("307163759.png")) {
-        // setTimeout(() => {
-        //   document.querySelector('.bottom > img').src = "./assets/img/Vector 451.png"
-        // }, 100)
-      } else {
-        document.querySelector('.bottom > img').src = "./assets/img/Group 307163759.png"
-      }
-    }
-  })
-  mc3.on('pandown', function () {
-    document.querySelector('.bottom__map-shtor').classList.remove('hidden')
-    document.querySelector('.bottom__forum-content ').classList.remove('bottom__forum-content--index')
-    document.querySelector('.bottom__forum .wrapper1 > img ').classList.remove('index')
-    // document.querySelector('.bottom > img').classList.remove('vis')
-    console.log(document.querySelector('.bottom__forum-dropdown--index'));
-    if (window.innerWidth > 501) {
-      if (document.querySelector('.bottom > img').src.includes("4521.png")) {
-        // setTimeout(() => {
-        //   document.querySelector('.bottom > img').src = "./assets/img/Vector 4522.png"
-
-        // }, 200)
-      } else {
-        document.querySelector('.bottom > img').src = "./assets/img//Vector 4521.png"
-        document.querySelector('.bottom > img').classList.remove('left')
-
-      }
-    } else {
-
-      if (document.querySelector('.bottom > img').src.includes("307163759.png")) {
-        setTimeout(() => {
-          document.querySelector('.bottom > img').src = "./assets/img/Vector 452.png"
-
-        }, 100)
-      } else {
-        document.querySelector('.bottom').classList.remove('bottom--bottom')
-        document.querySelector('.bottom > img').src = "./assets/img/Group 307163759.png"
-      }
-    }
-    document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--active')
-    if (!document.querySelector('.bottom__forum-dropdown--active')) {
-      console.log(1);
-      document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--index')
-      // document.querySelector('.bottom > img').classList.add('vis')
-      setTimeout(() => {
-        document.querySelector('.bottom__forum ').classList.remove('bottom__forum--index')
-      }, 150)
-    } else {
-      document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--index')
-      console.log(2);
-      setTimeout(() => {
+        document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--index')
         document.querySelector('.bottom__forum ').classList.add('bottom__forum--index')
-      }, 100)
-    }
-
-    if (window.innerWidth > 769) {
-      document.querySelector('.info').classList.remove('info--index')
-    } else {
-      document.querySelector('.info').classList.remove('info--opacity')
-    }
-    document.querySelector('.bottom__forum .wrapper1 > svg').classList.remove('active')
-
-  })
+        setTimeout(() => {
+  
+        }, 100)
+      }
+      console.log(document.querySelector('.bottom__forum--index'));
+      if (window.innerWidth > 769) {
+        document.querySelector('.info').classList.add('info--index')
+      } else {
+        document.querySelector('.info').classList.toggle('info--opacity')
+      }
+      document.querySelector('.bottom__forum .wrapper1 > svg').classList.toggle('active')
+      if (window.innerWidth > 501) {
+        setTimeout(() => {
+          document.querySelector('.bottom__backg').classList.add('bottom__backg--alt')
+          
+        }, 100)
+       
+      } else {
+       
+      }
+    })
+    mc3.on('pandown', function () {
+      document.querySelector('.bottom__map-shtor').classList.remove('hidden')
+      document.querySelector('.bottom__forum-content ').classList.remove('bottom__forum-content--index')
+      document.querySelector('.bottom__forum .wrapper1 > img ').classList.remove('index')
+      // document.querySelector('.bottom > img').classList.remove('vis')
+      console.log(document.querySelector('.bottom__forum-dropdown--index'));
+      if (window.innerWidth > 501) {
+        document.querySelector('.bottom__backg').classList.remove('bottom__backg--alt')
+        
+      } else {
+  
+          document.querySelector('.bottom').classList.remove('bottom--bottom')
+      
+      }
+      document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--active')
+      if (!document.querySelector('.bottom__forum-dropdown--active')) {
+        console.log(1);
+        document.querySelector('.bottom__forum-dropdown').classList.remove('bottom__forum-dropdown--index')
+        // document.querySelector('.bottom > img').classList.add('vis')
+        setTimeout(() => {
+          document.querySelector('.bottom__forum ').classList.remove('bottom__forum--index')
+        }, 150)
+      } else {
+        document.querySelector('.bottom__forum-dropdown').classList.add('bottom__forum-dropdown--index')
+        console.log(2);
+        setTimeout(() => {
+          document.querySelector('.bottom__forum ').classList.add('bottom__forum--index')
+        }, 100)
+      }
+  
+      if (window.innerWidth > 769) {
+        document.querySelector('.info').classList.remove('info--index')
+      } else {
+        document.querySelector('.info').classList.remove('info--opacity')
+      }
+      document.querySelector('.bottom__forum .wrapper1 > svg').classList.remove('active')
+  
+    })
+  }
   mc4.on('panup', function () {
     document.querySelector('.bottom').classList.add('bottom--top')
   })
